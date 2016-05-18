4e8fd8002f213e41393c01602390a662e5349447
Frederikam <frogkr@gmail.com>
2016-05-18 11:30:25 +0200

    Basic functionality

--- src/main/java/com/frederikam/fredboat/bootloader/Bootloader.java
+++ src/main/java/com/frederikam/fredboat/bootloader/Bootloader.java
@@ -1,9 +1,31 @@
 package com.frederikam.fredboat.bootloader;
 
+import java.io.IOException;
+
 public class Bootloader {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException, InterruptedException {
+        while (true) {
+            Process process = boot();
+            process.waitFor();
+            
+            if (process.exitValue() == ExitCodes.EXIT_CODE_UPDATE) {
+                update();
+            } else {
+                break;
+            }
+        }
+    }
+
+    public static Process boot() throws IOException {
+        ProcessBuilder pb = new ProcessBuilder("java -jar FredBoat-1.0.jar")
+                .inheritIO();
+        Process process = pb.start();
+        return process;
     }
     
+    public static void update(){
+        
+    }
+
 }
---
+++ src/main/java/com/frederikam/fredboat/bootloader/ExitCodes.java
@@ -0,0 +1,8 @@
+package com.frederikam.fredboat.bootloader;
+
+public class ExitCodes {
+    
+    public static final int EXIT_CODE_NORMAL = 0;
+    public static final int EXIT_CODE_UPDATE = 8056;
+    
+}
