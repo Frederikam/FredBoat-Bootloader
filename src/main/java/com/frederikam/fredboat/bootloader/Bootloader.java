b362c0cce934a9a44d67da9b7658decfc435dc76
Frederikam <frogkr@gmail.com>
2016-08-27 19:15:55 +0200

    Bootloader is now configurable

--- src/main/java/com/frederikam/fredboat/bootloader/Bootloader.java
+++ src/main/java/com/frederikam/fredboat/bootloader/Bootloader.java
@@ -1,26 +1,47 @@
 package com.frederikam.fredboat.bootloader;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Bootloader {
 
+    public static JSONArray command;
+    public static String jarName;
+    
     public static void main(String[] args) throws IOException, InterruptedException {
+        InputStream is = new FileInputStream(new File("./bootloader.json"));
+        Scanner scanner = new Scanner(is);
+        JSONObject json = new JSONObject(scanner.useDelimiter("\\A").next());
+        scanner.close();
+        
+        command = json.getJSONArray("command");
+        jarName = json.getString("jarName");
+        
+        OUTER:
         while (true) {
             Process process = boot();
             process.waitFor();
-
             System.out.println("[BOOTLOADER] Bot exited with code " + process.exitValue());
-
-            if (process.exitValue() == ExitCodes.EXIT_CODE_UPDATE) {
-                System.out.println("[BOOTLOADER] Now updating...");
-                update();
-            } else if (process.exitValue() == 130 || process.exitValue() == ExitCodes.EXIT_CODE_NORMAL) {
-                System.out.println("[BOOTLOADER] Now shutting down...");
-                break;
-                //SIGINT received or clean exit
-            } else {
-                System.out.println("[BOOTLOADER] Now restarting..");
+            
+            switch (process.exitValue()) {
+                case ExitCodes.EXIT_CODE_UPDATE:
+                    System.out.println("[BOOTLOADER] Now updating...");
+                    update();
+                    break;
+                case 130:
+                case ExitCodes.EXIT_CODE_NORMAL:
+                    System.out.println("[BOOTLOADER] Now shutting down...");
+                    break OUTER;
+                    //SIGINT received or clean exit
+                default:
+                    System.out.println("[BOOTLOADER] Now restarting..");
+                    break;
             }
         }
     }
@@ -28,22 +49,28 @@ public class Bootloader {
     public static Process boot() throws IOException {
         //ProcessBuilder pb = new ProcessBuilder(System.getProperty("java.home") + "/bin/java -jar "+new File("FredBoat-1.0.jar").getAbsolutePath())
         ProcessBuilder pb = new ProcessBuilder()
-                .command("java", "-jar", "FredBoatMusic-1.0.jar")
                 .inheritIO();
+        ArrayList<String> list = new ArrayList<>();
+        command.forEach((Object str) -> {
+            list.add((String) str);
+        });
+        
+        pb.command(list);
+        
         Process process = pb.start();
         return process;
     }
 
     public static void update() {
         //The main program has already prepared the shaded jar. We just need to replace the jars.
-        File oldJar = new File("./FredBoatMusic-1.0.jar");
+        File oldJar = new File("./" + jarName);
         oldJar.delete();
-        File newJar = new File("./update/target/FredBoatMusic-1.0.jar");
+        File newJar = new File("./update/target/" + jarName);
         newJar.renameTo(oldJar);
 
         //Now clean up the workspace
-        new File("./update").delete();
-        System.out.println("[BOOTLOADER] Updated");
+        boolean deleted = new File("./update").delete();
+        System.out.println("[BOOTLOADER] Updated. Update dir deleted: " + deleted);
     }
 
 }
